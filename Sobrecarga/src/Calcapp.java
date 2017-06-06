b6cb04301d529d6de6842f24eec27d064a22803d
Harold Flores <hflores@nubity.com>
2017-06-05 21:01:55 -0300

    Update code

--- Sobrecarga/src/Calcapp.java
+++ Sobrecarga/src/Calcapp.java
@@ -4,6 +4,18 @@ public class Calcapp {
 		Calculadora object=new Calculadora();
 		
 		System.out.print("Suma: ");
-		System.out.println(object.suma("6","5.4","10","67.98","190"));
+		System.out.println(object.suma("6","5.4","10","67.98","190")+"\n");
+
+		System.out.print("Suma: ");
+		System.out.println(object.suma("6","4")+"\n");
+
+		System.out.print("Suma: ");
+		System.out.println(object.suma("6","4","5")+"\n");
+
+		System.out.print("Suma: ");
+		System.out.println(object.suma("7.8","56.9")+"\n");
+
+		System.out.print("Suma: ");
+		System.out.println(object.suma("7.8","56.9","99.4")+"\n");
 	}
 }
